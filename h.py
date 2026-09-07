08546514df0d64c5a702ff9e38dbae0bc0412ed1
Prajwalt19 <titarmareprajwal19@gmail.com>
2026-09-07 17:02:01 +0530

    stop tracking h.py

--- h.py
+++
@@ -1,9 +0,0 @@
-import os
-from pathlib import Path
-
-cache = Path.home() / ".cache"
-
-for root, dirs, files in os.walk(cache):
-    for file in files:
-        if "ViT" in file or "clip" in file.lower() or file.endswith(".pt") or file.endswith(".bin"):
-            print(os.path.join(root, file))
